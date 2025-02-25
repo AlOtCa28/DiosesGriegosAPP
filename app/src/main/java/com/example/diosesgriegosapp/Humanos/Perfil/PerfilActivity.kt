85e341d67f44a640e0ee1f12aaee75407c0e1c53
AlOcA28 <alvaro.oc02@gmail.com>
2025-02-25 21:45:19 +0100

    CAMBIOS

--- app/src/main/java/com/example/diosesgriegosapp/Humanos/Perfil/PerfilActivity.kt
+++ app/src/main/java/com/example/diosesgriegosapp/Humanos/Perfil/PerfilActivity.kt
@@ -1,16 +1,23 @@
 package com.example.diosesgriegosapp.Humanos.Perfil
 
+import Parametros.Parametros
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.diosesgriegosapp.R
 import com.example.diosesgriegosapp.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
     lateinit var binding: ActivityPerfilBinding
+    private val viewModel: PerfilActivityViewModel by viewModels()
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +29,17 @@ class PerfilActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Parametros.usuarioLogeado?.let { viewModel.obtenerUsuarioPorNombreVM(it) }
+        viewModel.myResponse.observe(this) { usuario ->
+            usuario?.let {
+                binding.edtNombrePerfil.setText(it.nombre)
+                binding.edtEmailPerfil.setText(it.email)
+                binding.edtContraPerfil.setText(it.contraseña)
+                binding.edtDestinoNuevo.setText(it.destino.toString())
+            }
+        }
+
 
         binding.btnSalirPerfil.setOnClickListener {
             finish()
---
+++ app/src/main/java/com/example/diosesgriegosapp/Humanos/Perfil/PerfilActivityViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.diosesgriegosapp.Humanos.Perfil
+
+import API.UserNetwork
+import Modelo.Usuario.Usuario
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class PerfilActivityViewModel : ViewModel() {
+
+    private val _myResponse = MutableLiveData<Usuario?>()
+    val myResponse: LiveData<Usuario?> get() = _myResponse
+
+    private val _myResponseList = MutableLiveData<List<Usuario>>()
+    val myResponseList: LiveData<List<Usuario>> get() = _myResponseList
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    private val _resOperacion = MutableLiveData<Boolean>()
+    val resOperacion: LiveData<Boolean> get() = _resOperacion
+
+    private val _errorCode = MutableLiveData<Int?>()
+    val errorCode: LiveData<Int?> get() = _errorCode
+
+    fun obtenerUsuarioPorNombreVM(nombre: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            var response: Response<Usuario> = UserNetwork.retrofit.obtenerUsuarioPorNombre(nombre)
+
+            if (response.isSuccessful) {
+                _myResponse.value = response.body()
+            } else {
+                _myResponse.value = null
+                _errorCode.value = response.code()
+            }
+            _isLoading.value = false
+        }
+    }
+}
